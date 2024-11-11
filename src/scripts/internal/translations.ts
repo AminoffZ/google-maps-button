1f9ccfbb02135b3126efeb59427b2fceaac3899d
AminoffZ <mouiylus@gmail.com>
2024-11-11 10:40:17 +0200

    Add Translations

--- src/scripts/internal/translations.ts
+++ src/scripts/internal/translations.ts
@@ -8,17 +8,17 @@ export const TRANSLATIONS: Translations = {
     'Add Maps': 'Add Maps',
   },
   es: {
-    Maps: 'Maps',
+    Maps: 'Mapas',
     'Add Map': 'Añadir Map',
     'Add Maps': 'Añadir Mapa',
   },
   sv: {
-    Maps: 'Maps',
+    Maps: 'Kartor',
     'Add Map': 'Lägg till Karta',
     'Add Maps': 'Lägg till Kartor',
   },
   fr: {
-    Maps: 'Maps',
+    Maps: 'Plans',
     'Add Map': 'Ajouter Carte',
     'Add Maps': 'Ajouter Maps',
   },
@@ -26,5 +26,10 @@ export const TRANSLATIONS: Translations = {
     Maps: 'Térképek',
     'Add Map': 'Térkép hozzáadása',
     'Add Maps': 'Térképek hozzáadása',
+  },
+  fi: {
+    Maps: 'Kartat',
+    'Add Map': 'Lisää kartta',
+    'Add Maps': 'Lisää karttoja',
   },
 };
